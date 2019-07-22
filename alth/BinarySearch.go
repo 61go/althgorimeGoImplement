e0ad7a5ded7e1311774164b6f7df07d501e3457a
huruipeng <huruipeng@jd.com>
2019-07-22 09:38:37 +0800

    project init....

---
+++ alth/BinarySearch.go
@@ -0,0 +1,32 @@
+package alth
+/**
+如题二分查找法go语言实现
+ */
+type BinarySearch struct{}
+
+func (BinarySearch BinarySearch) Search(arr []int, target int) int {
+	n := len(arr)
+	if n == 0 {
+		return -1
+	}
+	return bs(arr, target, 0, n-1)
+
+}
+
+func bs(arr []int, target int, start int, end int) int {
+
+	if start >= end {
+		return -1
+	}
+	middle := (start + end) / 2
+	if arr[middle] == target {
+		return middle
+	}
+	if arr[middle] > target {
+		return bs(arr, target, start, middle)
+	}
+	if arr[middle] < target {
+		return bs(arr, target, middle, end)
+	}
+	return -1
+}
---
+++ home.go
@@ -0,0 +1,10 @@
+package main
+
+import "./alth"
+func main() {
+	arr:=[]int{1,2,3,4,5,6,7,8,9,10}
+	search := alth.BinarySearch{}
+	i := search.Search(arr, 8)
+	println("is :")
+	println(i)
+}
